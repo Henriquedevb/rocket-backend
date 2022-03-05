3cbbd442308ae8ecefa951f61ade69a6109117e0
Henrique Fontenele <henrique.fontenele@randon.com.br>
2022-03-05 19:32:20 -0300

    feat: created entiti and migration to users

---
+++ src/modules/accounts/entities/User.ts
@@ -0,0 +1,37 @@
+import { randomUUID } from 'crypto';
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity('users')
+class User {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  driver_license: string;
+
+  @Column()
+  isAdmin: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = randomUUID();
+    }
+  }
+}
+
+export { User };
