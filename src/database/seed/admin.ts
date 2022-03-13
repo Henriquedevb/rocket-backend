c91ea19b39672ace3d103aeb766d4eadbb32f602
Henrique Fontenele <henrique.fontenele@randon.com.br>
2022-03-12 23:11:32 -0300

    feat: create seed to admin and edit database connection

---
+++ src/database/seed/admin.ts
@@ -0,0 +1,21 @@
+import { getConnection } from 'typeorm';
+import { randomUUID } from 'crypto';
+import { hash } from 'bcrypt';
+import createConnection from '../index';
+
+async function create() {
+  const connection = await createConnection('localhost');
+
+  const id = randomUUID();
+  const password = await hash('admin', 8);
+
+  await connection.query(
+    `INSERT INTO USERS(id, name, email, password, "isAdmin", created_at, driver_license)
+        values('${id}', 'admin', 'admin@admin.com.br', '${password}', true, 'now()', 'XXX-XXXX')  
+        `
+  );
+
+  await connection.close();
+}
+
+create().then(() => console.log('User Admin Created.'));
