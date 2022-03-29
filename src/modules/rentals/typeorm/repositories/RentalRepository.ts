567f3261aa1f8c4fbed175751e8220abb5c66b4f
Henrique Fontenele <henrique.fontenele@randon.com.br>
2022-03-28 21:26:36 -0300

    feat: create devolution rental useCase

--- src/modules/rentals/typeorm/repositories/RentalRepository.ts
+++ src/modules/rentals/typeorm/repositories/RentalRepository.ts
@@ -37,6 +37,12 @@ class RentalRepository implements IRentalsRepository {
 
     return rental;
   }
+
+  async findById(id: string): Promise<Rental> {
+    const rental = await this.repository.findOne({ id });
+
+    return rental;
+  }
 }
 
 export { RentalRepository };
---
+++ src/modules/rentals/useCase/devolutionRental/DevolutionRentalUseCase.ts
@@ -0,0 +1,67 @@
+import { inject, injectable } from 'tsyringe';
+import { AppError } from '../../../../errors/AppErros';
+import { IDateProvider } from '../../../../shared/container/providers/DateProvider/IDateProvider';
+import { ICarsRepository } from '../../../cars/repositories/ICarsRepository';
+import { IRentalsRepository } from '../../repositories/IRentalsRepository';
+
+interface IRequest {
+  id: string;
+  user_id: string;
+}
+
+@injectable()
+class DevolutionControllerUseCase {
+  constructor(
+    @inject('RentalsRepository')
+    private rentalsRepository: IRentalsRepository,
+
+    @inject('CarsRepository')
+    private carsRepository: ICarsRepository,
+
+    @inject('DayjsDateProvider')
+    private dateProvider: IDateProvider
+  ) {}
+
+  async execute({ id, user_id }: IRequest) {
+    const rental = await this.rentalsRepository.findById(id);
+    const car = await this.carsRepository.findById(id);
+
+    if (!rental) {
+      throw new AppError('Rentals does not exists.');
+    }
+
+    const dateNow = this.dateProvider.dateNow();
+
+    let daily = this.dateProvider.compareInDays(
+      rental.start_date,
+      this.dateProvider.dateNow()
+    );
+
+    if (daily <= 0) {
+      daily = 1;
+    }
+
+    const delay = this.dateProvider.compareInDays(
+      dateNow,
+      rental.expected_return_date
+    );
+
+    let total = 0;
+    if (delay > 0) {
+      const calculate_fine = delay * car.fine_amount;
+      total = calculate_fine;
+    }
+
+    total += daily * car.daily_rate;
+
+    rental.end_date = this.dateProvider.dateNow();
+    rental.total = total;
+
+    await this.rentalsRepository.create(rental);
+    await this.carsRepository.updateAvailable(car.id, true);
+
+    return rental;
+  }
+}
+
+export { DevolutionControllerUseCase };
